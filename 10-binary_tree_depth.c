c1de4ec967b4f9c4ad2cf097bfb8ecd961455701
Joanfa7 <jafabre95@gmail.com>
2022-07-20 22:17:56 +0000

    fixing betty in 10-file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - measures the depth of a node 
+ * binary_tree_depth - measures the depth of a node
  * @tree: is a pointer to the node to measure the depth
  * Return: succes
  */
@@ -9,8 +9,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t deph;
 
-	if(tree == NULL || tree->parent == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 	deph = binary_tree_depth(tree->parent);
-	return(deph + 1);
+	return (deph + 1);
 }

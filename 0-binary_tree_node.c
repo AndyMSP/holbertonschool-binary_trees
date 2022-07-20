21834d8cc8ddcc8645aee6b93e4d2ffd3cc82c02
Joanfa7 <jafabre95@gmail.com>
2022-07-20 16:39:04 +0000

    fixing betty on 0-file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,16 +1,18 @@
 #include "binary_trees.h"
 
 /**
- *
- *
+ * binary_tree_node- creates a new binary tree
+ * @parent: first node
+ * @value: data that is inside the nodes
+ * return: success
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
-	
-	if(!(tmp))
-		return(NULL);
+
+	if (!(tmp))
+		return (NULL);
 
 	else
 	{
